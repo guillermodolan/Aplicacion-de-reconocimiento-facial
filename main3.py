bd9181895a058a793afb01f03331b04ff336d5d7
Eneasss <eneasparatore@gmail.com>
2023-10-09 09:38:52 -0300

    Mensajes por kivy (salvo muestra de archivos) + No muestra video

--- main3.py
+++ main3.py
@@ -1,15 +1,112 @@
 import os
+from os.path import isfile, join
+
 from kivy.app import App
-from kivy.properties import StringProperty, ListProperty
+from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
 import cv2
 from deepface import DeepFace
+from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
 import mediapipe as mp
 import time
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.popup import Popup
-from kivy.uix.image import Image
-import easygui
+from kivy.uix.image import Image, AsyncImage
+from kivy.uix.videoplayer import VideoPlayer
+
+
+class ArchivoScreen(Screen):
+
+    # Método para capturar el nombre de la foto, de la persona que se haya logueado. Esto se
+    # asignará al botón almacenado en la pantalla de 'archivo'
+
+    # Ejemplo: 'Guillermo.jpg', extrae solo el nombre Guillermo
+    def get_button_text(self):
+        # Verificar si la carpeta 'fotos' ya existe
+        carpeta_fotos = "fotos"
+        if not os.path.exists(carpeta_fotos):
+            # Si no existe, crear la carpeta
+            os.makedirs(carpeta_fotos)
+
+        # Con esto capturamos el nombre de la foto, de la persona que se haya logueado.
+        # Ejemplo: 'Guillermo.jpg', extraemos solo el nombre "Guillermo"
+        for nombre_archivo in os.listdir(carpeta_fotos):
+            if nombre_archivo.endswith(".jpg"):
+                nombre_sin_extension = os.path.splitext(nombre_archivo)[0]
+                return nombre_sin_extension  # Devuelve el nombre sin extensión
+
+    # Método que sirve para mostrar las imágenes.
+    def mostrar_archivos(self):
+        nombre = self.get_button_text()
+        carpeta_fotos_y_videos = f"fotos_y_videos/{nombre}.jpg"
+
+        # Verificar si la carpeta 'carpeta_fotos_y_videos' existe
+        if not os.path.exists(carpeta_fotos_y_videos):
+            print('No existe la carpeta')
+        else:
+            # Obtener una lista de archivos en la carpeta
+            archivos = [f for f in os.listdir(carpeta_fotos_y_videos) if isfile(join(carpeta_fotos_y_videos, f))]
+
+            # Limpiar el contenido anterior del ScrollView
+            self.ids.scroll_view.clear_widgets()
+
+            # Crear un layout para el ScrollView
+            layout = BoxLayout(orientation='vertical', spacing=10)
+
+            # Verificar si hay archivos en la carpeta
+            if archivos:
+                # Agregar cada archivo como un botón al layout
+                for archivo in archivos:
+                    btn = Button(text=archivo, size_hint_y=None, height=40)
+                    if any(ext in archivo.lower() for ext in ('.png', '.jpg', '.jpeg', '.gif')):
+                        # Si es una imagen, mostrar la imagen
+                        btn.bind(on_press=lambda instance, file=archivo: self.mostrar_imagen(file))
+                    elif any(ext in archivo.lower() for ext in ('.mp4', '.avi', '.mov', '.mkv')):
+                        # Si es un video, mostrar el video
+                        btn.bind(on_press=lambda instance, file=archivo: self.mostrar_video(file))
+                    layout.add_widget(btn)
+            else:
+                # Si no hay archivos, mostrar un mensaje
+                layout.add_widget(Label(text="No hay archivos en la carpeta."))
+
+            # Agregar el layout al ScrollView
+            self.ids.scroll_view.add_widget(layout)
+
+    def mostrar_imagen(self, archivo):
+        # Capturo el nombre de la persona logueada
+        nombre = self.get_button_text()
+
+        ruta_imagen = join(f"fotos_y_videos/{nombre}.jpg", archivo)
+
+        # Mostrar la imagen en un Popup
+        img = AsyncImage(source=ruta_imagen)
+        popup = Popup(title=archivo, content=img, size_hint=(None, None), size=(400, 400))
+        popup.open()
+
+    def mostrar_video(self, archivo):
+        # Capturo el nombre de la persona logueada
+        nombre = self.get_button_text()
+        ruta_video = join(f"fotos_y_videos/{nombre}", archivo)
+        ruta_modificada = ruta_video.replace('\\', '/')
+        try:
+            captura = cv2.VideoCapture(ruta_modificada)
+            while True:
+                ret, imagen = captura.read()
+                if ret:
+                    cv2.imshow('video', imagen)
+                else:
+                    # El video ha llegado al final
+                    break
+                if cv2.waitKey(30) == 27:  # Presionar la tecla 'Esc' para salir
+                    break
+        except Exception as e:
+            print(f"Ocurrió un error: {str(e)}")
+        finally:
+            captura.release()
+            cv2.destroyAllWindows()
 
 
 class LoginScreen(Screen):
@@ -20,21 +117,17 @@ class HomeScreen(Screen):
     welcome_text = StringProperty('')
 
 
+# Define la pantalla de registro
 class RegisterScreen(Screen):
     pass
 
 
-class FileScreen(Screen):
-    my_list_attribute = ListProperty([])
-
-    def on_my_list_attribute(self, instance, value):
-        rv = self.ids.rv  # Obtén el RecycleView de la pantalla
-        data = [{'archivo': archivo} for archivo in value]  # Crea los datos para el RecycleView
-        rv.data = data  # Actualiza los datos del RecycleView
+class TextoHomeScreen(Screen):
+    texto = StringProperty('')
 
 
-class BotonYArchivo(BoxLayout):
-    archivo = StringProperty('')
+class TextoLoginScreen(Screen):
+    texto = StringProperty('')
 
 
 class LoginApp(App):
@@ -46,92 +139,130 @@ class LoginApp(App):
         # Pantalla de inicio de sesión
         login_screen = LoginScreen(name='login')
 
+        # iniciar_sesion_button = Button(text='Iniciar Sesión')
+        # crear_cuenta_button = Button(text='Crear Cuenta')
+
+        # iniciar_sesion_button.bind(on_press=self.iniciar_sesion)
+        # crear_cuenta_button.bind(on_press=self.crear_cuenta)
+
+        # layout_login = BoxLayout(orientation='vertical', spacing=10, padding=10)
+        # layout_login.add_widget(iniciar_sesion_button)
+        # layout_login.add_widget(crear_cuenta_button)
+
+        # login_screen.add_widget(layout_login)
+
         # Pantalla principal (Home)
         home_screen = HomeScreen(name='home')
 
+        # camara_button = Button(text='Cámara')
+        # archivo_button = Button(text='Archivo')
+
+        # camara_button.bind(on_press=self.abrir_camara)
+        # archivo_button.bind(on_press=self.abrir_archivo)
+
+        # layout_home = BoxLayout(orientation='vertical', spacing=10, padding=10)
+
+        # layout_home.add_widget(camara_button)
+        # layout_home.add_widget(archivo_button)
+
+        # home_screen.add_widget(layout_home)
         # Pantalla de registro
         register_screen = RegisterScreen(name='register')
 
-        file_screen = FileScreen(name='files')
+        archivo_screen = ArchivoScreen(name='archivo')
+
+        texto_home_screen = TextoHomeScreen(name='text_home')
+
+        texto_login_screen = TextoLoginScreen(name='text_login')
 
         self.sm.add_widget(login_screen)
         self.sm.add_widget(home_screen)
         self.sm.add_widget(register_screen)
-        self.sm.add_widget(file_screen)
+        self.sm.add_widget(archivo_screen)
+        self.sm.add_widget(texto_home_screen)
+        self.sm.add_widget(texto_login_screen)
         self.sm.current = 'login'
 
         return self.sm
 
-    def iniciar_sesion(self):
-
+    def obtener_nombre(self):
+        # Verificar si la carpeta 'fotos' ya existe
         carpeta_fotos = "fotos"
         if not os.path.exists(carpeta_fotos):
             # Si no existe, crear la carpeta
             os.makedirs(carpeta_fotos)
 
+        # Con esto capturamos el nombre de la foto, de la persona que se haya logueado.
+        # Ejemplo: 'Guillermo.jpg', extraemos solo el nombre "Guillermo"
+        for nombre_archivo in os.listdir(carpeta_fotos):
+            if nombre_archivo.endswith(".jpg"):
+                nombre_sin_extension = os.path.splitext(nombre_archivo)[0]
+                return nombre_sin_extension  # Devuelve el nombre sin extensión
+
+    def iniciar_sesion(self):
+
         # Inicializar la cámara frontal
         cap = cv2.VideoCapture(0)
+
+        # Variable para almacenar el nombre de la foto con coincidencia
         foto_coincidente = None
 
-        # lista de nombres de archivo de las fotos en la carpeta "fotos"
+        # Obtener la lista de nombres de archivo de las fotos en la carpeta "foto"
         fotos_en_carpeta = os.listdir("fotos")
 
         while True:
+            # Capturar un fotograma de la cámara frontal
             ret, frame = cap.read()
+
+            # Mostrar el fotograma en una ventana
             cv2.imshow('Camara Frontal', frame)
 
             # Esperar a que el usuario presione la tecla 'q' para capturar la imagen
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
-        # reconocimiento facial en la imagen capturada y comparar con las fotos en la carpeta "fotos"
+        # Realizar el reconocimiento facial en la imagen capturada y comparar con las fotos en la carpeta "foto"
         try:
             for nombre_foto in fotos_en_carpeta:
                 ruta_foto = os.path.join("fotos", nombre_foto)
                 result = DeepFace.verify(ruta_foto, frame, model_name="Facenet", distance_metric='euclidean_l2')
 
                 if result["verified"]:
-                    easygui.msgbox(f"La cara en la imagen capturada coincide con la foto {nombre_foto}.",
-                                   "Inicio de Sesión Exitoso")
-                    # print(f"La cara en la imagen capturada coincide con la foto {nombre_foto}.")
                     foto_coincidente = nombre_foto
                     break  # Detener la búsqueda cuando se encuentra una coincidencia
 
             if foto_coincidente:
-                # Si hay una coincidencia, guarda el nombre de la foto coincidente
-                print("Foto coincidente:", foto_coincidente)
+                # Si hay una coincidencia, puedes guardar el nombre de la foto coincidente
                 self.sm.get_screen('home').welcome_text = foto_coincidente
                 # Cambia a la pantalla de inicio después de iniciar sesión
-                self.sm.current = 'home'
+                self.sm.get_screen('text_home').texto = f"La cara en la imagen capturada coincide con la foto {foto_coincidente}."
+                self.sm.current = 'text_home'
             else:
-                easygui.msgbox("La cara en la imagen capturada no coincide con ninguna foto en la carpeta 'foto'.",
-                               "Inicio de Sesión Fallido")
-                # print("La cara en la imagen capturada no coincide con ninguna foto en la carpeta 'foto'.")
+                self.sm.get_screen('text_login').texto = ("La cara en la imagen capturada no coincide con ninguna foto "
+                                                          "en la carpeta 'foto'.")
+                self.sm.current = 'text_login'
 
         except Exception as e:
-            error = str(e)
-            easygui.msgbox(error, "Error")
-            # print("Error:", str(e))
+            print("Error:", str(e))
 
         # Cerrar la ventana de la cámara
         cap.release()
         cv2.destroyAllWindows()
 
     def crear_cuenta(self):
-
-        carpeta_fotos = "fotos"
-        if not os.path.exists(carpeta_fotos):
-            # Si no existe, crear la carpeta
-            os.makedirs(carpeta_fotos)
+        # Agregar lógica para crear una cuenta
         self.sm.current = 'register'
 
     def abrir_camara(self, instance):
 
         mp_drawing = mp.solutions.drawing_utils
         mp_hands = mp.solutions.hands
+
         cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
         posicion_izquierda = (50, 50)
-        posicion_derecha = (50, 100)
+        posicion_derecha = (50, 100)  # Nueva posición para el texto de "Grabando"
+
         fuente = cv2.FONT_HERSHEY_SIMPLEX
         escala_fuente = 1
         color = (255, 255, 255)
@@ -163,24 +294,6 @@ class LoginApp(App):
                 frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 results = hands.process(frame_rgb)
 
-                if grabando and results.multi_hand_landmarks is None:
-                    # Detener la grabación del video
-
-                    end_video_time = time.time()
-                    video_duration = end_video_time - start_video_time
-                    if video_duration >= 2.0:
-                        video_output.release()
-                        grabando = False
-                        easygui.msgbox(f"Video guardado como {filename}", "mensaje")
-
-                        # print(f"Video guardado como {filename}")
-                    else:
-                        # Borrar el video si dura menos de 2 segundos
-                        video_output.release()
-                        os.remove(filename)
-                        grabando = False
-                        # print(f"Video borrado porque dura menos de 2 segundos")
-
                 if results.multi_hand_landmarks is not None:
                     for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
                         mp_drawing.draw_landmarks(
@@ -198,60 +311,46 @@ class LoginApp(App):
                             else:
                                 filename = os.path.join(output_directory, f"foto_{int(time.time())}.jpg")
                                 cv2.imwrite(filename, frame)
-                                easygui.msgbox(f"¡Foto tomada y guardada como {filename}!", "mensaje")
-
-                                # print(f"¡Foto tomada y guardada como {filename}!")
+                                print(f"¡Foto tomada y guardada como {filename}!")
                                 start_time = None
 
-                        if handedness.classification[0].label == "Right":
+                        elif handedness.classification[0].label == "Right":
+                            # Mostrar "Grabando" cuando se detecta la mano derecha
                             cv2.putText(frame, "Grabando", posicion_derecha, fuente, escala_fuente, color, 2)
                             if not grabando:
                                 # Iniciar la grabación del video
                                 filename = os.path.join(output_directory, f"video_{int(time.time())}.avi")
                                 video_output = cv2.VideoWriter(filename, fourcc, 20.0, (width, height))
                                 grabando = True
-                                start_video_time = time.time()
+                        else:
                             if grabando:
-                                # Agregar el frame al video en grabación
-                                video_output.write(frame)
-                        # else:
-                        if grabando and handedness.classification[0].label != "Right":
-                            # Detener la grabación del video
-
-                            end_video_time = time.time()
-                            video_duration = end_video_time - start_video_time
-                            if video_duration >= 2.0:
+                                # Detener la grabación del video
                                 video_output.release()
                                 grabando = False
-                                easygui.msgbox(f"Video guardado como {filename}", "mensaje")
+                                print(f"Video guardado como {filename}")
 
-                                # print(f"Video guardado como {filename}")
-                            else:
-                                # Borrar el video si dura menos de 2 segundos
-                                video_output.release()
-                                os.remove(filename)
-                                grabando = False
-                                print(f"Video borrado porque dura menos de 2 segundos")
+                if grabando:
+                    # Agregar el frame al video en grabación
+                    video_output.write(frame)
 
                 cv2.imshow("Frame", frame)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
 
+        # Liberar recursos al finalizar
+        if grabando:
+            video_output.release()
+
         cap.release()
         cv2.destroyAllWindows()
 
-    def listar_archivos(self, instance):
-        carpeta = "fotos_y_videos/" + instance
-
-        if not os.path.exists(carpeta):
-            # Si no existe, crear la carpeta
-            os.makedirs(carpeta)
-
-        # Lista los archivos en la carpeta especificada
-        archivos = os.listdir(carpeta)
-        # Agrega el archivo a la lista de archivos
-        self.sm.get_screen('files').my_list_attribute = archivos
-        self.sm.current = 'files'
+    def abrir_archivo(self):
+        # Obtener el resultado del método get_button_text()
+        texto_boton = self.sm.get_screen('archivo').get_button_text()
+        # Asignar el resultado al texto del botón
+        self.sm.get_screen('archivo').ids.btn_archivo.text = texto_boton if texto_boton is not None else 'Vacío'
+        # Cambiar a la pantalla de archivo
+        self.sm.current = 'archivo'
 
     # Agrega una función para tomar la foto y guardarla con el nombre
     def tomar_foto(self, instance):
@@ -281,58 +380,8 @@ class LoginApp(App):
                     self.sm.current = 'login'
                     break
         else:
-            print("Ingresa un nombre válido antes de tomar la foto.")
-
-    def abrir_fotos(self, archivo):
-        home_screen = self.sm.get_screen('home')
-        
-        carpeta = os.path.join("fotos_y_videos", home_screen.welcome_text)
-        ruta_completa = os.path.join(carpeta, archivo)
-
-        # extensión del archivo
-        extension = archivo.split('.')[-1].lower()
-
-        if extension == 'jpg' or extension == 'jpeg' or extension == 'png':
-            # Si es una foto, muestra la imagen
-            self.mostrar_imagen(ruta_completa)
-        elif extension == 'avi' or extension == 'mp4':
-            # Si es un video, reproduce el video
-            self.reproducir_video(ruta_completa)
-        else:
-            print("Tipo de archivo no compatible.")
-
-    def mostrar_imagen(self, ruta_completa):
-        try:
-            # Carga y muestra la imagen en una ventana emergente
-            imagen = Image(source=ruta_completa)
-            popup = Popup(title='Imagen', content=imagen, size_hint=(None, None), size=(400, 400))
-            popup.open()
-        except Exception as e:
-            print("Error al mostrar la imagen:", str(e))
-
-    def reproducir_video(self, ruta_completa):
-        # ruta completa esta guardada con este formato: fotos_y_videos\nombre.jpg\nombrevideo
-        cadena = ruta_completa
-        # le cambio las \ por /
-        cadena_modificada = cadena.replace('\\', '/')
-
-        try:
-            captura = cv2.VideoCapture(cadena_modificada)
-            while True:
-                ret, imagen = captura.read()
-                if ret:
-                    cv2.imshow('video', imagen)
-                else:
-                    # El video ha llegado al final
-                    break
-
-                if cv2.waitKey(30) == 27:  # Presionar la tecla 'Esc' para salir
-                    break
-        except Exception as e:
-            print(f"Ocurrió un error: {str(e)}")
-        finally:
-            captura.release()
-            cv2.destroyAllWindows()
+            self.sm.get_screen('text_login').texto = "Ingresa un nombre válido antes de tomar la foto."
+            self.sm.current = 'text_login'
 
 
 if __name__ == '__main__':
